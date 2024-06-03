0ee38fb4070260d8d7ee170c35f82caf2c4b96af
Yosra Mekaoui <yosra.mekaoui@esprit.tn>
2024-06-03 17:10:03 +0200

    loginnn

--- src/app/shared/components/login/login.component.ts
+++ src/app/shared/components/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../../core/services/auth.service';
 import { AuthResponse } from '../../../modules/auth-response.model';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -15,6 +16,8 @@ export class LoginComponent {
   connectButtonText = 'Connect';
   email: string='';
   password: string='';
+  errorMessage: string = '';
+
 
   constructor(private authService: AuthService, private router: Router) {}
 
@@ -25,8 +28,12 @@ export class LoginComponent {
         localStorage.setItem('token', response.token);
         this.router.navigate(['/']);
       },
-      error => {
-        console.error(error);
+      (error: HttpErrorResponse) => {
+        if (error.status === 401) {
+          this.errorMessage = 'Invalid email or password';
+        } else {
+          this.errorMessage = 'An error occurred. Please try again later.';
+        }
       }
     );
   }
